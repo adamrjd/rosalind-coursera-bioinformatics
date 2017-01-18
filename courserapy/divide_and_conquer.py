b5310b0392a477d79bf34c886c497178ec4d5389
Adam Dornford <Adam Dornford>
2017-01-18 13:01:23 -0500

    commit

--- courserapy/divide_and_conquer.py
+++ courserapy/divide_and_conquer.py
@@ -1,4 +1,4 @@
-'''Written in Python36'''
+'''Written in Python3.5'''
 from dataimport import Score
 
 
@@ -10,28 +10,26 @@ def show(matrix):
 def linear_scoring(v, w, sigma, score_dict):
     '''Linear scoring for first half'''
     # initialize and seed with penalties
-    # this time we are actually splitting the vertical matrix
-    # and adding one row at a time vertically
-    s = [[-sigma * _] for _ in range(len(v) + 1)]
-    backtrack = [None for _ in range(len(v) + 1)]
+    # dictionaries are much more efficient here since we
+    # will still end up with very large data structures
+    s = {(_, 0): -sigma * _ for _ in range(len(v) + 1)}
+    backtrack = {_: None for _ in range(len(v) + 1)}
 
     # scoring
     for y in range(1, len(w) + 1):
         for x in range(len(v) + 1):
             if x == 0:
-                s[x].append(-sigma * y)
+                s[(x, y)] = -sigma * y
             else:
-                scores = (s[x - 1][1] - sigma,  # right
-                          # compensate for len being 1 starting off
-                          s[x][len(s[x]) - 1] - sigma,  # down
-                          s[x - 1][0] + score_dict[v[x - 1]][w[y - 1]])  # diag
+                scores = (s[(x, y - 1)] - sigma,  # right
+                          s[(x - 1, y)] - sigma,  # down
+                          s[(x - 1, y - 1)] + score_dict[v[x - 1]][w[y - 1]])  # diag
 
                 score = max(scores)
-                s[x].append(score)
+                s[(x, y)] = score
                 backtrack[x] = scores.index(score)
-
-            if len(s[x]) > 2:
-                s[x].pop(0)
+            if y - 2 >= 0:
+                s.pop((x, y - 2))
 
     return s, backtrack
 
@@ -48,23 +46,23 @@ def middle_edge(v, w, sigma, score_dict):
         jcol = len(w) // 2 if len(w) > 2 else 1
 
         # scoring matrices for left and right
-        rfoldr = lambda l: list(l)[::-1] if isinstance(l, list) else l
         s = linear_scoring(v, w[:jcol], sigma, score_dict)[0]
-        r_s, backtrack = [
-            [rfoldr(_) for _ in __] for __ in
-            linear_scoring(v[::-1], w[jcol:][::-1], sigma, score_dict)]
+        r_s, r_backtrack = linear_scoring(
+            v[::-1], w[jcol:][::-1], sigma, score_dict)
 
         max_score = -float('inf')
         node = None
-        for x in range(len(w) + 1):
-            score = s[x][1] + r_s[x][0]
+        # len(v) // 2 + 1 approximate bounds for 'longest path' since sometimes
+        # the max score occurs after the node with the longest path
+        for x in range(len(v) // 2 + 1):
+            score = s[(x, jcol)] + r_s[(len(v) - x, jcol)]
             if score > max_score:
                 node, max_score = [(x, jcol), score]
 
         dirs = [tuple(map(sum, zip(node, _)))
                 for _ in [(1, 0), (0, 1), (1, 1)]]
 
-        return (node, dirs[backtrack[node[0]]])
+        return (node, dirs[r_backtrack[len(v) - node[0]]])
     return (None, None) if len(w) <= 1 else get_edge(v, w, sigma, score_dict)
 
 
@@ -83,7 +81,7 @@ class LinearSpaceAlignment(object):
         self.score_dict = score_dict
 
     def global_align(self):
-        def recurse(self, left, right, top, bottom):
+        def recurse(self, left, right, top, bottom, flag=False):
             from dynamic_programming import global_alignment
             # right block empty, want to now align this segment
             if top == bottom:
@@ -92,13 +90,16 @@ class LinearSpaceAlignment(object):
             elif left == right:
                 return ['-' * (bottom - top), self.w[top:bottom]]
 
-            # trivial cases exit recursion here
+            # trivial cases
             elif bottom - top == 1 or right - left == 1:
                 return global_alignment(self.sigma, self.score_dict,
                                         self.v[left:right], self.w[top:bottom])[1:]
             else:
                 node, edge = middle_edge(
                     self.v[left:right], self.w[top:bottom], self.sigma, self.score_dict)
+
+                if node is None:
+                    return ['', '']  # end of recursion
 
                 # shift nodes from local source to global source
                 node = tuple(map(sum, zip(node, [left, top])))
@@ -112,13 +113,22 @@ class LinearSpaceAlignment(object):
 
                 # generate next subalignment from left and right subtrees
                 A = recurse(self,
-                            left, node[0], top, node[1])
+                            left, node[0], top, node[1], flag)
+
+                # shift edge with current alignment...but only want to do this for
+                # all subparts of righthand side from original entry to
+                # recursive tree; honestly this is such a hack lol
+                if (left, right, top, bottom) == (0, len(self.v), 0, len(self.w)):
+                    flag = True
+                if flag:
+                    edge = tuple(edge[_] - 1 if current[_] == '-' else edge[_] + 1
+                                 for _ in range(2))
 
                 B = recurse(self,
                             edge[0], right,
-                            edge[1], bottom)
+                            edge[1], bottom, flag)
 
-                # zip left and right together recursively
+                # zip left and right together recursively...
                 return [A[i] + current[i] + B[i] for i in range(2)]
 
         self.v, self.w = recurse(self, 0, len(self.v), 0, len(self.w))
@@ -131,11 +141,13 @@ class LinearSpaceAlignment(object):
         pass
 
 if __name__ == "__main__":
-    str1 = 'TWLNSACYGVNFRRLNPMNKTKWDCWTWVPMVMAAQYLCRIFIPVMDHWEFFGDWGLETWRLGIHDHVKIPNFRWSCELHIREHGHHFKTRFLKHNQFTQCYGLMPDPQFHRSYDVACQWEVTMSQGLMRFHRQNQIEKQRDRTSTYCMMTIGPGFTSNGYDPFVTITITPVQEPVENWFTPGGSMGFMIISRYMQMFFYLTRFSDMTYLVGVHCENYVCWNNVAKFLNGNLQGIFDQGERAYHQFVTWHSYSQYSRCSVGRYACEQAMSRVNSKMTWHWPIRDQGHEHFSEQYLSEKRNPPCNPRIGNAGQHFYEIHRIAHRVAMCNWAPQGQHPGGPTPHDVETCLWLWSLCLKGSDRGYVDRPWMFLADQLGEANLTLITMFHGCTRGCLMWFMDWEECVCSYSVVNPRCHGSEQWSVQNLGWRTCDTLISLWEPECDKHNTPPCLHWEFEDHPSQLRPVMMCDKYVQSIPTDAKWAWTYSKDFVISHWLIWTPIKLEECVFPQINRLWGTACNQGSQKIVIQNVWLRPSSFFQERSKCSDSSCILNVGGSNVNITGKETRTHVPILHMHEIDLISTASSGMRHNLILPHGMLMLHMNWHHSTRAMNPYSSLKLIPWTFQVCETDDRDQNVATHVADPCHKGEDQEIRCCKGGVDHQWKGDRMWMMCMPDMNYVKQDQAPSGTCEGACENYPADKDKCYMIFTIVFDYRRCTKKVCIWISGFPVDAFNLISIANAGFFCCWLEPTELKWRRTFYLGKGTQGWMCTFPHRNIIPVIICAGFGRWVQGEVPFRPVAQISAHSSDRRQGHHPPGTNMCHDYGDQYPIKRVGMQVEEDDGASYCDCAADWKLADMYEADHLSIGVIDFTDWIYPKNGGIWSEIIKSHFHWYHWETPQNTVGAFNTIVGINGSDMCIYHGNTQWEFGWCWKWLNHGHMRNQGPCHLGILEGRISKFAQVTSWWWQTKHDKDWSIEPYGRHWGEAGRPYTYNYCWMRWAIVYNHGNVISVELVPFMDEYPGKCNKEDVQFELFSPMQA'
-    str2 = 'LWFKFLQCIFQYFKDQQETNCIWTFSPFSEHICQRVCQVYWNWNTPSSRTSDPRELFANSTIHNNRCGEWRYMFYHTRTLVQTAPLMKETLHSDGKHSMYCEQRHFFRSSYLIKVNYDVSHYLELYTFSEIPWKLTTHGWDGFSWFLLVNSCCTFDIDGKCGILSQCGMSRAFRTRQEDAYHFQTSLMHLHLHLHVQEGKHEKADLFAQFYNMLPMHGGTCGRNTEPSDLFDSATMNKYMAEHPASCKACPNVSKECFVYWWSHDFTKKHKLIEFSCGRDTGQTTQRTWNVDENEGGKWIWRFHYFMRAKALQIDPKFKPYWNEPRAIMRPGHVTAAPCICAQHSQNETAVCNRDQMHIHAIEFQQYHSRAFGEVQTWCDIGKENENDFIYEQHWWLVGGTEGMAGVIWKFVCARCRTQDCDFWKTCLTYSAQPMMKVYDTIFYVNSINPWEFEDHPSQCDKCVQSIPTDAKYAICGKFVISHWLYWTPQKFEECVHNNVRCAPMGNRLWGTACMVIQNVWLRPSMGSHFSCILNVGGSNINIQGKETWTHVPILHMHEIDLISTASSGMETCKPCFLSGPTIHMGFSYEIRAQPYSRDYFCMDWMQEADEVDHNRCETVQPTLPLLQQFEWKTSCMGQRWITIFCDHCQIVCFSTFFCVMPTFLPNTSILDKFYCIYLSISWTHYCNVHALGFIMRLHYSYMGWKEHKRMHAWDIGLDELWAQEGIQRAQLWCGDEFEVAKYPEWITEARTAIATRPWFHNCYIKPWWIREKHLWFGKESKLDHGHRGAMFTPVANDNTEWMHHWYMFCWAGSKNRLKRQIKEKLIFIIKFMITEFGLFLMIDYTQCYIAWMWAYTGIACYIDWEKCLKHDLTTTDLGCCVYRLFKWYEVRHRAPPQVNTRLPWSQIPMVAIQCNIVDECKEQWHFSYKASFVVEYLCPGCCTNGNRWQWYQVKETPFMYAFAASIFGFHHENLVVFITGSVTIPNGLFGCIAWTSPKPVQKTPASANTIIAYDKCILMG'
+    with open('input.txt', 'r') as f:
+        str1, str2 = [line.strip() for line in f.readlines()]
+
     penalty = 5
     score_matrix = Score().BLOSUM62
-    print(middle_edge(str1, str2, penalty, score_matrix))
-    '''answer = LinearSpaceAlignment(
+    answer = LinearSpaceAlignment(
         str1, str2, penalty, score_matrix).global_align()
-    print(answer)'''
+    with open('output.txt', 'w') as f:
+        for _ in answer:
+            f.write(str(_) + '\n')
